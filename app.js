bb404b38b531af9e878d8e4e704e88eb0de4405e
Olly Clements <olcl@pml.ac.uk>
2017-09-11 08:51:50 +0100

    some glider bits

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.get('/getdistance/:_cmdr', function (req, res) {
 })
 
 app.listen(3000, function () {
-  //console.log('Example app listening on port 3000!')
+  console.log('Example app listening on port 3000!')
 })
 
 
